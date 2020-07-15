273c89d43e367c1239ccb5f23757e0328490c855
rick <rhennessey3@gmail.com>
2020-07-15 14:53:43 -0400

    welp

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,28 @@
+BEGIN;
+
+INSERT INTO blogful_articles (id, title, content, date_published)
+VALUES
+    ('1', 'Cloudapp', 'Research from Stanford shows that speech is 3x faster than text. So record a quick video rather than send the long email.', 2020-07-15 22:10:25-04),
+    ('2', 'Codewars', 'Is n divisible by x and y?, 2020-07-15 22:10:25-04),
+    ('3', 'Creative circle', 'Fullstack engineering positions near you', 2020-07-15 22:10:25-04),
+    ('4', 'Noun project', 'noun gazette july inspiration.', 2020-07-15 22:10:25-04),
+    ('5', 'project management institute', 'The next generation of future-focused project professionals are', 2020-07-15 22:10:25-04),
+    ('6', 'Scrimba',  ' Youll learn how to create a declarative, reusable component library for your team ',2020-07-15 22:10:25-04),
+    ('7', 'Postgres', 'Valid input for the time stamp types consists of the concatenation of a date and a time, followed by an optional time zone, followed by an optional AD or BC.  ',2020-07-15 22:10:25-04),
+    ('8', 'Dribble', 'the team that sweats together stays together ',2020-07-15 22:10:25-04),
+    ('9', 'greensock', 'greensock 3.4 released. check out the new features here ',2020-07-15 22:10:25-04),
+    ('10', 'economist', 'a really catchy title goes in here ',2020-07-15 22:10:25-04),
+    ('11', 'UGI', 'pay your bill bro ! this is a link to pay your bill ',2020-07-15 22:10:25-04),
+    ('12', 'warby parker', 'I like glasses because i have to wear glasses unless i got lasic ',2020-07-15 22:10:25-04),
+    ('13', 'Word Genius', 'Word of the day is factotum , noun an employee who does all kinds of work. like me',2020-07-15 22:10:25-04),
+    ('14', 'loom', 'sending and reciving videos is easy with this tool and helps me do code reviews faster ',2020-07-15 22:10:25-04),
+    ('15', 'acquia', 'i am researching this as a possible tool to use for a client. It enables personalized content on a 1 to 1 basis. ',2020-07-15 22:10:25-04),
+    ('16', 'x.ai', 'if you are like me and hate finding a time to schedule meetings with busy people this is the tool for you ',2020-07-15 22:10:25-04),
+    ('17', 'stitcher', 'was a service that i used to listen to podcasts on my sonos until spotify started taking that over. i dont miss it, heres why ',2020-07-15 22:10:25-04),
+    ('18', 'nextdoor digest', 'tries to be a social network for neighberhoods. its really just a chain email spam bot. this is why i dont use it and 4 suggestions for a better user expierence ',2020-07-15 22:10:25-04),
+    ('19', 'Comcast', 'is the devil and its taking over the world, here are 4 solutions to broadband access in the greater philadelphia area ',2020-07-15 22:10:25-04),
+    ('20', 'IIBA', 'this is an organization for business analyists. business analysis are helpful when developing product requirements in complex organizations ',2020-07-15 22:10:25-04)
+
+;
+
+COMMIT;
